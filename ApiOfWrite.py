6d9a554acfd6a059c99bb958d75a1e7a412bb636
Daniel <yang05051@outlook.com>
2021-02-28 15:53:57 +0800

    Update ApiOfWrite.py

--- ApiOfWrite.py
+++ ApiOfWrite.py
@@ -303,17 +303,17 @@ for _ in range(1,config['rounds']+1):
         print('上传文件')
         with open(xlspath,'rb') as f:
             uploadFile(a,filesname,f)
-        choosenum = random.sample(range(1, 5),2)
+        choosenum = random.sample(range(1, 4),2)
         if config['allstart'] == 1 or 1 in choosenum:
             print('excel文件操作')
             excelWrite(a,filesname,'QVQ'+str(random.randint(1,600)))
         #if config['allstart'] == 1 or 2 in choosenum:
         #    print('team操作')
         #    teamWrite(a,'QVQ'+str(random.randint(1,600)))
-        if config['allstart'] == 1 or 3 in choosenum:
+        if config['allstart'] == 1 or 2 in choosenum:
             print('task操作')
             taskWrite(a,'QVQ'+str(random.randint(1,600)))
-        if config['allstart'] == 1 or 4 in choosenum:
+        if config['allstart'] == 1 or 3 in choosenum:
             print('onenote操作')
             onenoteWrite(a,'QVQ'+str(random.randint(1,600)))
         print('-')
